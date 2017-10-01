58ee76ad45c1bb5bc0a21c501b3fe9feb05f9509
Enam Shah <enamshah09@gmail.com>
2017-09-30 18:26:52 -0700

    Word Break Optimized

---
+++ WordBreakOptimized.java
@@ -0,0 +1,40 @@
+/**
+ * 139. Word Break
+ * 
+ * Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+ * You may assume the dictionary does not contain duplicate words.
+ * 
+ * For example, given
+ * 
+ * s = "leetcode",
+ * dict = ["leet", "code"].
+ * Return true because "leetcode" can be segmented as "leet code".
+ */
+
+Code:
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        
+        if(s==null)
+        {
+            return true;
+        }
+        
+        boolean arr2[]=new boolean[s.length()+1];
+        arr2[0]=true;
+        
+        for(int i=1;i<=s.length();i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(arr2[j] && wordDict.contains(s.substring(j,i)))
+                {
+                    arr2[i]=true;
+                    break;
+                }
+            }
+        }
+        return arr2[s.length()];
+    }
+}
